588d120e9cddb39a9b4dab8723ce5e4370f9e2dd
pouet <nchrupal@student.42.fr>
2017-04-13 10:49:33 +0200

    fix d07
    Signed-off-by: pouet <nchrupal@student.42.fr>

--- d07/ex02/Array.hpp
+++ d07/ex02/Array.hpp
@@ -1,6 +1,8 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
+
 template< typename T >
 class Array {
 	public:
@@ -11,10 +13,69 @@ class Array {
 
 		Array< T > & operator = (Array< T > const & src);
 		T & operator [] (unsigned int b);
+		T const & operator [] (unsigned int b) const;
 
 	private:
 		T * _tab;
 		unsigned int _size;
 };
+
+template < typename T >
+Array< T >::Array(void)
+{
+	_size = 0;
+	_tab = new T[0];
+}
+
+template < typename T >
+Array< T >::Array(unsigned int sz)
+{
+	_size = sz;
+	_tab = new T[sz];
+}
+
+template < typename T >
+Array< T >::Array(Array const & rhs)
+{
+	_size = rhs._size;
+	_tab = new T[_size];
+
+	for (unsigned int i = 0; i < _size; i++)
+		_tab[i] = rhs._tab[i];
+}
+
+template < typename T >
+Array< T >::~Array(void)
+{
+	delete [] _tab;
+}
+
+template < typename T >
+Array< T > & Array< T >::operator = (Array< T > const & src)
+{
+	_size = src._size;
+	_tab = new T[_size];
+
+	for (unsigned int i = 0; i < _size; i++)
+		_tab[i] = src._tab[i];
+
+	return *this;
+}
+
+template < typename T >
+T & Array< T >::operator [] (unsigned int b)
+{
+	if (_size == 0 || b >= _size)
+		throw std::exception();
+	return _tab[b];
+}
+
+template < typename T >
+T const & Array< T >::operator [] (unsigned int b) const
+{
+	if (_size == 0 || b >= _size)
+		throw std::exception();
+	return _tab[b];
+}
 
 #endif
--- d07/ex02/main.cpp
+++ d07/ex02/main.cpp
@@ -1,9 +1,36 @@
+#include <iostream>
 #include "Array.hpp"
 
 int main(void)
 {
 	Array<int> a(10);
-//	Array<int> a(10U);
+	Array<int> b;
+
+	try {
+		std::cout << b[0] << std::endl;
+	}
+	catch (std::exception & e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	for (int i = 0; i < 10; i++)
+		a[i] = i * 10;
+
+	b = a;
+
+	for (int i = 0; i < 10; i++)
+		std::cout << a[i] << " " << b[i] << std::endl;
+
+	Array<double> c(10);
+	Array<double> d;
+
+	for (int i = 0; i < 10; i++)
+		c[i] = i * 0.5;
+
+	d = c;
+
+	for (int i = 0; i < 10; i++)
+		std::cout << c[i] << " " << d[i] << std::endl;
 
 	return 0;
 }
